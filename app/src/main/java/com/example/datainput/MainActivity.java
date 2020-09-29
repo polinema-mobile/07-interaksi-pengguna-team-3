24f455e938b388eade33fbc7acd6b7b00c125494
RhevyLisAlfianny <rhevylis48@gmail.com>
2020-09-29 09:26:12 +0700

    DatePicker

--- app/src/main/java/com/example/datainput/MainActivity.java
+++ app/src/main/java/com/example/datainput/MainActivity.java
@@ -3,16 +3,22 @@ package com.example.datainput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,13 +28,44 @@ public class MainActivity extends AppCompatActivity {
     RadioButton rb_laki, rb_perempuan;
     Spinner spinnerJurusan;
     Button Simpan;
-
+    DatePickerDialog picker;
+    EditText eText;
+    Button btnGet;
+    TextView tvw;
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvw=(TextView)findViewById(R.id.textView1);
+        eText=(EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+        btnGet=(Button)findViewById(R.id.button1);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvw.setText("Selected Date: "+ eText.getText());
+            }
+        });
 
         edtNama = (EditText)findViewById(R.id.edtNama);
         edtNim = (EditText)findViewById(R.id.edtNim);
@@ -38,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
         rb_perempuan = (RadioButton) findViewById(R.id.rb_perempuan);
         spinnerJurusan = (Spinner)findViewById(R.id.spinnerJurusan);
         Simpan = (Button) findViewById(R.id.Simpan);
+
+
 
         List<String> ListJurusan = new ArrayList<String>();
         ListJurusan.add("AKUNTANSI");
@@ -49,5 +88,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, ListJurusan);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerJurusan.setAdapter(adapter);
+
+
     }
 }
